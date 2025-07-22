45341ede9f5be1c0ce1d059ce7514508d9424a02
Daniels Šatcs <dev@dshatz.com>
2025-07-23 01:03:22 +0300

    Init

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "kobweb-cli flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in rec {
+        packages = rec {
+          kobweb-cli = pkgs.stdenv.mkDerivation rec {
+            name = "kobweb-cli";
+            version = "0.9.18";
+            src = builtins.fetchurl {
+              url = "https://github.com/varabyte/kobweb-cli/releases/download/v${version}/kobweb-${version}.tar";
+              sha256 = "sha256:1whz5qsqfbqickzafp6jpv0ckh39lr3ssqzksmgp5cwc6i38ff06";
+            };
+            unpackPhase = ''
+              tar -xf $src "kobweb-${version}"
+              mv "kobweb-${version}" source
+            '';
+
+            installPhase = ''
+              mkdir -p $out/bin
+              cp -r source/* $out/
+              chmod +x $out/bin/kobweb
+            '';
+            # Gradle dependency hash. Set to empty to rebuild.
+          };
+          default = kobweb-cli;
+        };
+
+        defaultPackage = packages.default;
+      }
+    );
+}
